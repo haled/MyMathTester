58fc23d448ddf6b33122316e1ffd5ced5202bea9
Darren Hale <darrenhale@darrens-mbp.attlocal.net>
2015-01-09 21:08:53 -0600

    Implemented a "problem" object to carry the operands around.

--- README.md
+++ README.md
@@ -5,9 +5,12 @@ A Google extension to drill basic math skills.
 
 To Do
 -----
+1. Refactor model in "web" stuff to be a more complex object.
 1. Add other basic math operations:
   a. ~~Multiplication~~
-  a. Subtraction
+  a. ~~Subtraction~~
+  a. Ensure subtraction operation has operand1 > operand2.
   a. Division
+1. Add unit testing.
 1. Convert the program from a Google extension to a Google app.
 
--- js/math_tester_controller.js
+++ js/math_tester_controller.js
@@ -5,33 +5,26 @@ mathTester.controller('MathTesterController', function ($scope) {
   $scope.problem = 'Addition';
 
   var mathProblem = setOperands($scope.problem);
-  $scope.operand1 = mathProblem.operand1;
-  $scope.operand2 = mathProblem.operand2;
-  $scope.operator = mathProblem.operator;
+  $scope.mathProblem = mathProblem;
 
   $scope.setResult = function() {
     if($scope.problem == 'Addition') {
-      $scope.result = checkAddition($scope.operand1, $scope.operand2, $scope.answer);
+      $scope.result = checkAddition($scope.mathProblem.operand1, $scope.mathProblem.operand2, $scope.answer);
     }
     else if($scope.problem == 'Multiplication') {
-      $scope.result = checkMultiplication($scope.operand1, $scope.operand2, $scope.answer);
+      $scope.result = checkMultiplication($scope.mathProblem.operand1, $scope.mathProblem.operand2, $scope.answer);
     }
     else if($scope.problem == 'Subtraction') {
-      $scope.result = checkSubtraction($scope.operand1, $scope.operand2, $scope.answer);
+      $scope.result = checkSubtraction($scope.mathProblem.operand1, $scope.mathProblem.operand2, $scope.answer);
     }
     $scope.answer = "";
     
     var mathProblem = setOperands($scope.problem);
-    $scope.operand1 = mathProblem.operand1;
-    $scope.operand2 = mathProblem.operand2;
-    $scope.operator = mathProblem.operator;
+    $scope.mathProblem = mathProblem;
   }
 
   $scope.setProblemType = function(probType) {
     var mathProblem = setOperands($scope.problem);
-    $scope.operand1 = mathProblem.operand1;
-    $scope.operand2 = mathProblem.operand2;
-    $scope.operator = mathProblem.operator;
+    $scope.mathProblem = mathProblem;
   }
 });
-
